406108b97279a7f90d6cb5df24f9c55f1e7bce8d
violetevergarden233 <878582581@qq.com>
2021-01-19 16:42:53 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,46 @@
+# '''
+# print("hello world!") # 字符串
+# print(2333) # 整数
+# print(2.333) # 小数
+# print(True) # 布尔值 True False
+# print(()) # 元组
+# print([]) # 数组
+# print({}) # 字典
+
+# 锄禾日当午
+# 汗滴禾下土
+
+# print('哈哈',2333)
+# print('嘻嘻'+'哈哈') # 字符串的拼接
+# print('哈哈'*100)
+# print(((1+2)*100-99)/2)
+# print(2<3)
+
+# a = '张三' # 把张三这个值赋值给了名字叫a的这个变量
+# print(a)
+# '''
+
+# # a = input('请输入：')
+# # print('input获取的内容：',a)
+
+# print(type('哈哈哈'))
+# print(type(233))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# 计算两个数之和
+# a = float(input('第一个数:'))
+# b = float(input('第二个数:'))
+# print('两个数的和是：',a+b)
+
+# a = 'wqddsdsfadaffawlfkwl;fjwqlfnwqe'
+# print(len(a))
+
+#通过代码获取两端内容，并且计算他们长度的和
+a = len(input('第一段内容：'))
+b = len(input('第二段内容：'))
+print('它们的长度为：',a+b)
